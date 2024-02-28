5de420b3514e632585eaa35dc431c95fe1ce0abe
ferrierf <florian.ferrier@aol.fr>
2024-02-28 13:57:01 +0100

    Fix pointer register for peripheral accessing

--- src/user_led.c
+++ src/user_led.c
@@ -1,47 +1,48 @@
 #include "user_led.h"
 #include <stdint.h>
 
+typedef struct { 
+  volatile uint32_t MODER;
+  volatile uint32_t OTYPER;
+  volatile uint32_t OSPEEDR;
+  volatile uint32_t PUPDR;
+  volatile uint32_t IDR;
+  volatile uint32_t ODR; 
+} gpio_s;
+
 #ifndef UTEST
 #define RCC_AHB1ENR (*(volatile uint32_t *)0x40023830)
-#define GPIOA_MODER (*(volatile uint32_t *)0x40020000)
-#define GPIOA_OTYPER (*(volatile uint32_t *)0x40020004)
-#define GPIOA_OSPEEDR (*(volatile uint32_t *)0x40020008)
-#define GPIOA_PUPDR (*(volatile uint32_t *)0x4002000C)
-#define GPIOA_ODR (*(volatile uint32_t *)0x40020014)
+#define GPIOA (*(gpio_s*)(0x40020000))
 #else
-uint32_t RCC_AHB1ENR;
-uint32_t GPIOA_MODER;
-uint32_t GPIOA_OTYPER;
-uint32_t GPIOA_OSPEEDR;
-uint32_t GPIOA_PUPDR;
-uint32_t GPIOA_ODR;
+extern volatile uint32_t RCC_AHB1ENR;
+extern gpio_s GPIOA;
 #endif
 
 void userLed_setup(void) {
     RCC_AHB1ENR |= (0b1 << 0); // Enable periph clock for GPIOA port
 
 	/* GPIOA5 as Output */
-	GPIOA_MODER &= ~(0b11 << 10); // Reset configuration mode for pin 5
-	GPIOA_MODER |= (0b01 << 10); // Set Output mode for pin 5
+	GPIOA.MODER &= ~(0b11 << 10); // Reset configuration mode for pin 5
+	GPIOA.MODER |= (0b01 << 10); // Set Output mode for pin 5
 
 	/* GPIOA5 on Push-Pull */
-	GPIOA_OTYPER &= ~(0b1 << 5);
+	GPIOA.OTYPER &= ~(0b1 << 5);
 
 	/* GPIOA5 on Low speed */
-	GPIOA_OSPEEDR &= ~(0b11 << 10);
+	GPIOA.OSPEEDR &= ~(0b11 << 10);
 
 	/* GPIOA5 on No pull-up/pull-down */
-	GPIOA_PUPDR &= ~(0b11 << 10);
+	GPIOA.PUPDR &= ~(0b11 << 10);
 }
 
 void userLed_set(userLed_state_e state) {
 	switch (state)
 	{
 	case USER_LED_STATE_OFF:
-		GPIOA_ODR &= ~(0b1 << 5);
+		GPIOA.ODR &= ~(0b1 << 5);
 		break;
 	case USER_LED_STATE_ON:
-		GPIOA_ODR |= (0b1 << 5);
+		GPIOA.ODR |= (0b1 << 5);
 		break;
 	default:
 		break;
--- test/ut/test_user_led.c
+++ test/ut/test_user_led.c
@@ -6,34 +6,39 @@
 
 #include "../../src/user_led.h"
 
-extern uint32_t RCC_AHB1ENR;
-extern uint32_t GPIOA_MODER;
-extern uint32_t GPIOA_OTYPER;
-extern uint32_t GPIOA_OSPEEDR;
-extern uint32_t GPIOA_PUPDR;
-extern uint32_t GPIOA_ODR;
+typedef struct { 
+  volatile uint32_t MODER;
+  volatile uint32_t OTYPER;
+  volatile uint32_t OSPEEDR;
+  volatile uint32_t PUPDR;
+  volatile uint32_t IDR;
+  volatile uint32_t ODR; 
+} gpio_s;
+
+volatile uint32_t RCC_AHB1ENR = 0;
+gpio_s GPIOA = {0};
 
 static void test_userLed_setup(void **state) {
     (void) state;
     userLed_setup();
     assert_int_equal(RCC_AHB1ENR, 0x00000001);
-    assert_int_equal(GPIOA_MODER, 0x00000400);
-    assert_int_equal(GPIOA_OTYPER, 0x00000000);
-    assert_int_equal(GPIOA_OSPEEDR, 0x00000000);
-    assert_int_equal(GPIOA_PUPDR, 0x00000000);
-    assert_int_equal(GPIOA_ODR, 0x00000000);
+    assert_int_equal(GPIOA.MODER, 0x00000400);
+    assert_int_equal(GPIOA.OTYPER, 0x00000000);
+    assert_int_equal(GPIOA.OSPEEDR, 0x00000000);
+    assert_int_equal(GPIOA.PUPDR, 0x00000000);
+    assert_int_equal(GPIOA.ODR, 0x00000000);
 }
 
 static void test_userLed_set_off(void **state) {
     (void) state;
     userLed_set(USER_LED_STATE_OFF);
-    assert_int_equal(GPIOA_ODR, 0x00000000);
+    assert_int_equal(GPIOA.ODR, 0x00000000);
 }
 
 static void test_userLed_set_on(void **state) {
     (void) state;
     userLed_set(USER_LED_STATE_ON);
-    assert_int_equal(GPIOA_ODR, 0x00000020);
+    assert_int_equal(GPIOA.ODR, 0x00000020);
 }
 
 int main(void) {
