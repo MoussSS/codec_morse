fa0c7f05c8ac94a8d43c4d83f31c1bee11dc3f0d
ferrierf <florian.ferrier@aol.fr>
2024-03-11 17:12:13 +0100

    Add SysTick handler to have a 1 ms counter

--- src/main.c
+++ src/main.c
@@ -9,12 +9,22 @@
 #include "mock_delay.h"
 #endif
 
+#define STK_CTRL (*(volatile uint32_t *)0xE000E010)
+#define STK_RELOAD (*(volatile uint32_t *)0xE000E014)
+#define STK_VAL (*(volatile uint32_t *)0xE000E018)
+#define STK_CALIB (*(volatile uint32_t *)0xE000E01C)
+
+extern volatile uint32_t systick_counter;
+
 /* Blink User Led on Nucleo-f446re board
  * User Led = GPIOA5
  */
 int MAIN(void) {
 
 	/* After reset, the CPU clock frequency is 16MHz */
+
+    STK_CTRL |= 0x00000007;
+    STK_RELOAD = 16000u; // each 1 ms
 
 	userLed_setup();
 
--- src/startup.c
+++ src/startup.c
@@ -14,12 +14,18 @@ extern uint32_t _la_data;
 extern uint32_t _sbss;
 extern uint32_t _ebss;
 
+uint32_t volatile systick_counter = 0;
+
 void Reset_Handler(void);
 void NMI_Handler(void);
 void HardFault_Handler(void);
 void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
 
 uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
     (uint32_t)STACK_START,
@@ -33,6 +39,11 @@ uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
 	(uint32_t) 0, // reserved
 	(uint32_t) 0, // reserved
 	(uint32_t) 0, // reserved
+	(uint32_t) &SVC_Handler,
+	(uint32_t) &DebugMon_Handler,
+	(uint32_t) 0, // reserved
+	(uint32_t) &PendSV_Handler,
+	(uint32_t) &SysTick_Handler,
 };
 
 void Reset_Handler(void) {
@@ -73,4 +84,20 @@ void BusFault_Handler(void) {
 
 void UsageFault_Handler(void) {
 	while(1);
+}
+
+void SVC_Handler(void) {
+	while(1);
+}
+
+void DebugMon_Handler(void) {
+	while(1);
+}
+
+void PendSV_Handler(void) {
+	while(1);
+}
+
+void SysTick_Handler(void) {
+	systick_counter++;
 }
