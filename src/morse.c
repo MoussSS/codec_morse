e8905b6d85b6ecea6bc13a1a2bf9e0ef2d096de9
florian ferrier <florian.ferrier@aol.fr>
2024-11-19 17:26:41 +0100

    Encode a full message

--- src/morse.c
+++ src/morse.c
@@ -3,13 +3,14 @@
 #include "morse.h"
 
 #define TIME_TO_CYCLE_FACTOR 10
-#define MORSE_REFERENCE_DURATION_MS 200
+#define MORSE_REFERENCE_DURATION_MS 150
 #define MAX_PART_IN_CHARACTER 5
 
 static const uint8_t DOT_NB_OF_CYCLE = MORSE_REFERENCE_DURATION_MS / TIME_TO_CYCLE_FACTOR;
 static const uint8_t DASH_NB_OF_CYCLE = DOT_NB_OF_CYCLE * 3;
 static const uint8_t SPACE_BETWEEN_PARTS_NB_OF_CYCLE = DOT_NB_OF_CYCLE;
 static const uint8_t SPACE_BETWEEN_LETTERS_NB_OF_CYCLE = DOT_NB_OF_CYCLE * 3;
+static const uint8_t SPACE_BETWEEN_WORDS_NB_OF_CYCLE = DOT_NB_OF_CYCLE * 7;
 
 static uint8_t cycle = 0;
 static uint8_t step = 0;
@@ -74,7 +75,7 @@ static uint8_t signal_duration = 0;
 static bool encode_morse_blank(void) {
     set_morse_signal(false);
     cycle++;
-    if (cycle >= SPACE_BETWEEN_LETTERS_NB_OF_CYCLE) {
+    if (cycle >= signal_duration) {
         cycle = 0;
     }
 
@@ -109,19 +110,9 @@ static bool encode_morse_data(void) {
     return (step < (encoded_char_sending.nb_of_part * 2));
 }
 
-static bool sending_char = false;
+static bool send_char = false;
 
 static bool send_character (char char_to_send) {
-    bool busy;
-    
-    if (sending_char) {
-        busy = encode_morse_data();
-    } else {
-        busy = encode_morse_blank();
-    }
-
-    if (!busy) {
-        if (!sending_char) {
             if ((char_to_send >= '0') && (char_to_send <= '9')) {
                 encoded_char_sending = ENCODED_NUMBERS[char_to_send - '0'];
             } else if ((char_to_send >= 'A') && (char_to_send <= 'Z')) {
@@ -129,49 +120,35 @@ static bool send_character (char char_to_send) {
             } else if ((char_to_send >= 'a') && (char_to_send <= 'z')) {
                 encoded_char_sending = ENCODED_LETTERS[char_to_send - 'a'];
             }
-        sending_char = true;
-        } else {
-            sending_char = false;
-        }
 
-    }
-
-    return busy;
+    return encode_morse_data();
 }
 
+static char message[] = "GROS CAca Boudin";
+static uint8_t char_index = 0;
+
 void encode_morse_message(void) {
-    send_character('z');
-#if 0    
-    cycle++;
-    switch (step) {
-        case 0:
-        set_morse_signal(true);
-        if (cycle >= DASH_NB_OF_CYCLE) {
-            cycle = 0;
-            step++;
-        }
-        break;
-        case 1:
-        set_morse_signal(false);
-        if (cycle >= SPACE_BETWEEN_PARTS_NB_OF_CYCLE) {
-            cycle = 0;
-            step++;
-        }
-        break;
-        case 2:
-        set_morse_signal(true);
-        if (cycle >= DOT_NB_OF_CYCLE) {
-            cycle = 0;
-            step++;
-        }
-        break;
-        case 3:
-        set_morse_signal(false);
-        if (cycle >= SPACE_BETWEEN_LETTERS_NB_OF_CYCLE) {
-            cycle = 0;
-            step = 0;
-        }
-        break;
+    bool busy;
+   
+    if (send_char) {
+        busy = send_character(message[char_index]);
+    } else {
+        busy = encode_morse_blank();
     }
-    #endif
+
+    if (!busy) {
+        if (send_char) {
+            send_char = false;
+            char_index++;
+            if (message[char_index] == ' ') {
+                signal_duration = SPACE_BETWEEN_WORDS_NB_OF_CYCLE;
+                char_index++;
+            } else {
+                signal_duration = SPACE_BETWEEN_LETTERS_NB_OF_CYCLE;
+             }
+        } else {
+            if (message[char_index] != 0)
+                send_char = true;
+        }
+    }
 }
